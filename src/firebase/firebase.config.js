0701b488090a8b08e52432e1f5828657359f897f
Amzad100 <mdamzad2003a@gmail.com>
2023-05-19 21:20:57 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBtyNfnHDGjcViqSvrAZVEpo6-mnLkr-dI",
-    authDomain: "toy-cars-c5c05.firebaseapp.com",
-    projectId: "toy-cars-c5c05",
-    storageBucket: "toy-cars-c5c05.appspot.com",
-    messagingSenderId: "1052287828992",
-    appId: "1:1052287828992:web:03f61c8a3eab79bdc9c4e5"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
